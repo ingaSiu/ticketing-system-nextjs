cf45369b9f1edd2660f4c10cf9af732073db7039
ingaSiu <ingasiud@gmail.com>
2025-06-23 11:06:39 +0300

    add function: decrypt and verify token

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import { logEvent } from '@/utils/sentry';
 const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
 const cookieName = 'auth-token';
 
+// Encrypt and sign token
 export async function signAuthToken(payload: any) {
   try {
     const token = await new SignJWT(payload)
@@ -19,5 +20,17 @@ export async function signAuthToken(payload: any) {
     logEvent('Token signing failed', 'auth', { payload }, 'error', error);
 
     throw new Error('Token signing failed');
+  }
+}
+
+// Decrypt and verify token
+export async function verifyAuthToken<T>(token: string): Promise<T> {
+  try {
+    const { payload } = await jwtVerify(token, secret);
+    return payload as T;
+  } catch (error) {
+    logEvent('Token decryption failed', 'auth', { tokenSnippet: token.slice(0, 10) }, 'error', error);
+
+    throw new Error('Token decryption failed');
   }
 }
