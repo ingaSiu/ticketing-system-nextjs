0e260c6776c3c77ee33e8709713106bb6995e47f
ingaSiu <ingasiud@gmail.com>
2025-06-26 08:24:19 +0300

    add user authentication check to TicketsPage component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import { getCurrentUser } from '@/lib/current-user';
 import { getPriorityClass } from '@/utils/ui';
 import { getTickets } from '@/actions/ticket.actions';
+import { redirect } from 'next/navigation';
 
 const TicketsPage = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect('/login');
+  }
   const tickets = await getTickets();
 
   return (
