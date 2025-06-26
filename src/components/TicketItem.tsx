9617f3b906fada7a36319a3bc1205182c4e1a2a7
ingaSiu <ingasiud@gmail.com>
2025-06-26 09:03:06 +0300

    refactor TicketItem to conditionally apply styles based on ticket status

--- src/components/TicketItem.tsx
+++ src/components/TicketItem.tsx
@@ -6,10 +6,13 @@ type TicketItemProps = {
   ticket: Ticket;
 };
 const TicketItem = ({ ticket }: TicketItemProps) => {
+  const isClosed = ticket.status === 'Closed';
   return (
     <div
       key={ticket.id}
-      className="flex justify-between items-center bg-white rounded-lg shadow border border-gray-200 p-6"
+      className={`flex justify-between items-center bg-white rounded-lg shadow border border-gray-200 p-6 ${
+        isClosed ? 'opacity-50' : ''
+      }`}
     >
       {/* Left Side */}
       <div>
@@ -23,7 +26,11 @@ const TicketItem = ({ ticket }: TicketItemProps) => {
 
         <Link
           href={`/tickets/${ticket.id}`}
-          className="inline-block mt-2 bg-blue-600 text-white text-sm px-3 py-1 rounded hover:bg-blue-700 transition text-center"
+          className={`inline-block mt-2  text-sm px-3 py-1 rounded  transition text-center ${
+            isClosed
+              ? 'bg-gray-400 text-gray-700 cursor-not-allowed pointer-events-none'
+              : 'bg-blue-600 text-white hover:bg-blue-700'
+          }`}
         >
           View Ticket
         </Link>
