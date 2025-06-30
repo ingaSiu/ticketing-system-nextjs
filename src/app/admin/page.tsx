039c7505bb190bbf1103c9dc62716e496e5dde5f
ingaSiu <ingasiud@gmail.com>
2025-06-30 14:26:26 +0300

    create dashboard page for admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,25 @@
+import TicketItem from '@/components/TicketItem';
 import { getCurrentUser } from '@/lib/current-user';
+import { getTickets } from '@/actions/ticket.actions';
 import { redirect } from 'next/navigation';
 
 const DashboardPage = async () => {
   const user = await getCurrentUser();
+  const tickets = await getTickets();
 
   if (!user || user.role !== 'ADMIN') {
     redirect('/login');
   }
 
-  return <div>DashboardPage</div>;
+  return (
+    <>
+      <div className="space-y-4 max-w-2xl mx-auto mt-6 ">
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket.id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default DashboardPage;
